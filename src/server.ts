fdf6f5c874200224ae21cd62e9a2576a0989b3b8
Jonathan Alves <jonathan.calves74@gmail.com>
2026-09-10 23:17:08 -0300

    fix: environment variable and config cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import fastify from 'fastify';
 import cors from '@fastify/cors';
 import { env } from './env';
@@ -8,9 +9,10 @@ import z from 'zod';
 export const app = fastify();
 
 app.register(cors, {
-  origin: [env.FRONTEND_LOCAL_URL, env.FRONTEND_PROD_URL],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  credentials: true
+  origin: [
+    'https://bellapizza-client.vercel.app',
+    'http://localhost:5173'
+  ]
 })
 
 app.get('/orders', async (req, reply) => {
@@ -234,7 +236,7 @@ app.get('/products', async () => {
 
 app
   .listen({
-    port: env.PORT,
+    port: Number(process.env.PORT) ?? 3333,
     host: '0.0.0.0'
   })
   .then(() => {
